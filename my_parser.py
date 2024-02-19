bf30f167aca237b7eb0b042d6c934a45fcd02ffa
stars869 <uz1qpuuz@gmail.com>
2024-02-19 17:15:02 -0500

    add parsing blocks and bindings

--- my_parser.py
+++ my_parser.py
@@ -1,7 +1,40 @@
 import typing as t
+from itertools import chain
 from dataclasses import dataclass
 from parser_combinator.parser_combinator import Parser, charPredPG, oneCharPG, stringPG
 
+
+def sepBlocks(s: str) -> list[str]:
+    lines = s.split('\n')
+    blocks = []
+    currentBlock = []
+
+    for line in lines:
+        if not line:
+            continue
+        if not line[0].isspace():
+            blocks.append("".join(currentBlock))
+            currentBlock = [line,]
+        else:
+            currentBlock.append(line)
+
+    blocks.append("".join(currentBlock))
+
+    blocks = list(filter(lambda x: x, blocks))
+    blocks = list(filter(lambda x: not x.isspace(), blocks))
+
+    return blocks
+
+def sepBinding(s: str) -> t.Tuple[str, str]:
+    result = s.split('=')
+    assert(len(result) == 2)
+
+    varname = result[0].strip() 
+    expr = result[1].strip()
+    assert(varname != "")
+    assert(expr != "")
+
+    return (varname, expr)
 
 @dataclass
 class Expr:
@@ -14,26 +47,38 @@ class Abstraction(Expr): pass
 
 class Application(Expr): pass
 
-alphabetP = charPredPG(lambda c: c.isalpha())
+def parseExpr(s: str) -> Expr:
+    alphabetP = charPredPG(lambda c: c.isalpha())
+    wsP = charPredPG(lambda c: c.isspace()) ** 0
+    lambdaSignP = wsP >> oneCharPG("\\") << wsP 
+    arrowSignP = wsP >> stringPG("->") << wsP
+    variableP = (wsP >> alphabetP ** 1 << wsP) ^ (lambda xs: Variable("".join(xs)))
+    paranLP = wsP >> oneCharPG("(") << wsP
+    paranRP = wsP >> oneCharPG(")") << wsP
+    abstractionLazyP = lambda: ((lambdaSignP >> variableP ** 1 << arrowSignP) + lambdaExprLazyP) ^ Abstraction
+    applicationLazyP = lambda: (((paranLP >> lambdaExprLazyP << paranRP) | abstractionLazyP | variableP) ** 2) ^ Application
+    lambdaExprLazyP = lambda: applicationLazyP() | (paranLP >> lambdaExprLazyP << paranRP) | abstractionLazyP | variableP
 
-wsP = charPredPG(lambda c: c.isspace()) ** 0
+    (expr, rest) = lambdaExprLazyP().parse(s)
+    assert(rest == "")
+    return expr
 
-lambdaSignP = wsP >> oneCharPG("\\") << wsP 
+def parseFile(path: str) -> list[t.Tuple[str, Expr]]:
+    file = open("./test.lambda", "r")
+    code = file.read()
+    file.close()
 
-arrowSignP = wsP >> stringPG("->") << wsP
+    blocks = sepBlocks(code)
 
-variableP = (wsP >> alphabetP ** 1 << wsP) ^ (lambda xs: Variable("".join(xs)))
-
-paranLP = wsP >> oneCharPG("(") << wsP
-
-paranRP = wsP >> oneCharPG(")") << wsP
-
-abstractionLazyP = lambda: ((lambdaSignP >> variableP ** 1 << arrowSignP) + lambdaExprLazyP) ^ Abstraction
-
-applicationLazyP = lambda: (((paranLP >> lambdaExprLazyP << paranRP) | abstractionLazyP | variableP) ** 2) ^ Application
-
-lambdaExprLazyP = lambda: applicationLazyP() | (paranLP >> lambdaExprLazyP << paranRP) | abstractionLazyP | variableP
+    bindings = []
+    for block in blocks:
+        (varname, exprCode) = sepBinding(block)
+        expr = parseExpr(exprCode)
+        bindings.append((varname, expr))
+    return bindings
 
 if __name__ == "__main__":
-    lambda_code = open("./test.lambda", "r").read()
-    print(lambdaExprLazyP().parse(lambda_code))
+    for (b, e) in parseFile("test.lambda"):
+        print(b)
+        print(e)
+        print()
